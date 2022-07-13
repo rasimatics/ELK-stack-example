dd026620dceecab3dc51b4dd55d83c61f6e6d021
Rasim <rasimmammadoff1@gmail.com>
2022-07-13 14:42:59 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import logging
+
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
+def setup_logger(name: str, log_file: str, level, formatter):
+
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    
+    return logger
+
+
+logger = setup_logger('test', r"C:\Users\Rasim\Desktop\Example app\logs\test.log", logging.INFO, formatter)
+
+
+# for i in range(10):
+#     if i % 3 == 0:
+#         logger.error("Error message")
+#     elif i % 2 == 0:
+#         logger.info("Info message")
+#     else:
+#         logger.warning("Warning message")
+
+logger.critical("Critical message")
